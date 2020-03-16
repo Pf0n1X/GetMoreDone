f194ff5e8d30bc64bd7d17c5d7f4381eb804920b
Pf0n1X <bboypsyfly@gmail.com>
2020-03-16 19:38:55 +0200

    - Finished settinga

--- app/src/main/java/com/pf0n1x/getmoredone/SettingsAboutActivity.java
+++ app/src/main/java/com/pf0n1x/getmoredone/SettingsAboutActivity.java
@@ -3,12 +3,25 @@ package com.pf0n1x.getmoredone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class SettingsAboutActivity extends AppCompatActivity {
+
+    // Data Members
+    TextView mAboutTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_about);
+
+        mAboutTextView = findViewById(R.id.textview_about);
+
+        setLinksClickable();
+    }
+
+    public void setLinksClickable() {
+        mAboutTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
